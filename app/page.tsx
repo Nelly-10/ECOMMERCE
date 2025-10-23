ea537e34f7ac0cade17775dbd4cdec6401e1e613
Nelly-10 <nelson_nonso@yahoo.com>
2025-10-23 16:19:14 +0100

    Second push

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 
+
+import Carousel from "@/components/carousel";
 import { Button } from "@/components/ui/button";
 import { stripe } from "@/lib/stripe";
+import Image from "next/image";
 import Link from "next/link";
 
 
@@ -11,22 +14,46 @@ export default async function Home() {
     limit: 5,
   });
 
-  
+
 
   // console.log(products);
   return (
     <div>
-        <section>
-          <div>
-             <div> 
-                <h2>Welcome to my Ecommerce</h2>
-                <p>Discover the latest products at the best prices</p>
-                <Button asChild variant="default">
-                  <Link href="/products">Browse All Products</Link>
-                </Button>
-             </div>
+      <section className="rounded bg-neutral-100 py-8 sm:py-12">
+        <div className="mx-auto grid grid-cols-1 items-center justify-items-center gap-8 px-8 sm:px-16 md:grid-cols-2">
+          <div className="max-w-md space-y-4">
+            <h2 className="text-3xl font-bold tracking-tight md:text-4xl">
+              Welcome to My Ecommerce
+            </h2>
+            <p className="text-neutral-600">
+              Discover the latest products at the best prices.
+            </p>
+            <Button
+              asChild
+              variant="default"
+              className="inline-flex items-center justify-center rounded-full px-6 py-3 bg-black text-white"
+            >
+              <Link
+                href="/products"
+                className="inline-flex items-center justify-center rounded-full px-6 py-3"
+              >
+                Browse All Products
+              </Link>
+            </Button>
           </div>
-        </section>
+          <Image
+            alt="Hero Image"
+            src={products.data[0].images[0]}
+            className="rounded h-auto w-auto"
+            width={450}
+            height={450}
+            priority
+          />
+        </div>
+      </section>
+      <section className="py-8">
+        <Carousel products={products.data} />
+      </section>
     </div>
   )
 }
